55f337b01305dcda04c0176e3ca09ce1b4bd9dbe
JoaquinDev <joaquin@isla.io>
2014-02-07 10:53:24 +0100

    Add super test, g and ../server/app.js and express
    Add super test, g and ../server/app.js and express
    
    Contact: joaquin@isla.io

--- test/testApi.spec.js
+++ test/testApi.spec.js
@@ -4,6 +4,12 @@ process.env.NODE_ENV = 'test';//A reference to your environment
 
 var should      = require('chai').should();
 var supertest   = require('supertest');
+var q           = require('q');
+var app         = require('../server/app').app;
+var api         = supertest(app);
+var userID;
+var badUserID;
+var taskID;
 
 //Start test (remembering tests structure)
 describe('Array', function(){
