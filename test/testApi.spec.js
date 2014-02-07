0493a7299530779bf51411b7a3c2383565904b50
JoaquinDev <joaquin@isla.io>
2014-02-07 11:00:52 +0100

    Test error Can't call method 'address' ofundefined
    Test error Can't call method 'address' ofundefined. When executing grunt
    test we get this error:
    
    "TypeError: Cannot call method 'address' of undefined". This is because
    we are defining at the begining of the testApi.spec.js this:
    
    var app = require('../server/app').app,
        api = supertest(app);
    
    This means that supertest is trying to connect but we still haven't type
    any code in the server/app.js file
    
    Contact: joaquin@isla.io

--- test/testApi.spec.js
+++ test/testApi.spec.js
@@ -12,13 +12,34 @@ var badUserID;
 var taskID;
 
 //Start test (remembering tests structure)
-describe('Array', function(){
+/*describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       [1,2,3].indexOf(5).should.equal(-1);
       [1,2,3].indexOf(0).should.equal(-1);
     })
   })
-})
+})*/
 
+/*describe('API definition', function(){
+  it('Returns available methods', function(done){
+    api.get('/')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function(res){ should.exist(res.body);})
+      .end(function(err, res){
+        if (err) return done(err);
+        res.body.should.have.property('methods');
+        done();
+      });
+  });
+});
+*/
+
+describe('2 API definition', function(){
+  it('Returns available methods', function(done){
+    api.get('/')
+  });
+});
+//Error: TypeError: Cannot call method 'address' of undefined
 
