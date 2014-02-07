5cbc5903870149248fb1a7a5b9157f693fb84863
JoaquinDev <joaquin@isla.io>
2014-02-07 11:10:02 +0100

    Add express server
    Add express server. Now supertest does not fail because we have our
    express server up and running at 3000. However we get a timeout error
    because we need to define the API routes
    
    Contact: joaquin@isla.io

--- server/app.js
+++ server/app.js
@@ -1,1 +1,26 @@
 var express = require('express');
+var app     = express(); 
+var http    = require('http'); 
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+
+if ('development' === app.get('env')){
+  app.use(express.errorHandler());
+}
+else if('test' === app.get('env')){
+  app.use(express.errorHandler());
+  database = 'test';
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Listening on ' + app.get('port'));
+});
+
+exports.app = app;
